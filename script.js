a5badb9116c76bf0f3f14702a6e7de075283d45e
Moran <morant@Morans-MacBook-Pro.local>
2017-09-16 23:14:35 -0400

    Add browser language to MailChimp's BLANG field.

--- script.js
+++ script.js
@@ -5,6 +5,13 @@ $(document).ready(function() {
   var language = navigator.languages && navigator.languages[0] || // Chrome / Firefox
                   navigator.language ||   // All browsers
                   navigator.userLanguage; // IE <= 10
+                
+  // update MailChimp's browser language field
+  if (language) {
+    $('#mc-embedded-subscribe-form').attr('action', function(i, val){
+      return val + '&BLANG=' + language;
+    });
+  }
 
   // default to english
   if (!language || supported_lang.indexOf(language.substr(0,2)) == -1){
